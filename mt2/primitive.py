0f519f2d55daf39cc4966de5760ee118cdda3d74
Bridger Herman <scooteeroo@gmail.com>
2018-03-19 17:25:06 -0500

    Add prime printing

--- mt2/primitive.py
+++ mt2/primitive.py
@@ -14,8 +14,11 @@ def factor(n):
 
 def is_primitive(x, p):
     for q in factor(p - 1):
-        if len(factor(q)) == 0 and (x**((p - 1)/q) % p) == 1:
-            return False
+        if len(factor(q)) == 0:
+            v = (x**((p - 1)/q) % p)
+            #  print('testing {}**(({} - 1)/{}) % {} = {}'.format(x, p, q, p, v))
+            if v == 1:
+                return False
     return True
 
 # Primitive elements, modulo m
