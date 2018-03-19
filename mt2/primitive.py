cb2abbca6850186a9275812b22dda2479fc86d90
Bridger Herman <scooteeroo@gmail.com>
2018-03-19 14:47:48 -0500

    Print out primitive elements

--- mt2/primitive.py
+++ mt2/primitive.py
@@ -14,7 +14,13 @@ def factor(n):
 
 def is_primitive(x, p):
     for q in factor(p - 1):
-        if len(factor(q)) == 0 and (x*((p - 1)/q) % p) == 1:
+        if len(factor(q)) == 0 and (x**((p - 1)/q) % p) == 1:
             return False
     return True
 
+# Primitive elements, modulo m
+def primitives_of(m):
+    assert len(factor(m)) == 0
+    for i in range(1, m):
+        if is_primitive(i, m):
+            print(i)
