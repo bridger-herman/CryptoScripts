25bf8b2d446e04d31979ea27418513fc21433a95
Bridger Herman <scooteeroo@gmail.com>
2018-03-18 22:33:29 -0500

    Start work on RSA

---
+++ mt2/euler.py
@@ -0,0 +1,5 @@
+# Euler Totient Function
+
+# phi(n) = n*\Pi_{p|n}{1 - 1/p}
+def phi(n):
+    pass
---
+++ mt2/rsa.py
@@ -0,0 +1,38 @@
+# RSA Encryption
+# Bridger Herman
+# March 2018
+
+import sys
+sys.path.append('../mt1')
+
+from euclid import egcd
+
+# msg = message
+# n = encryption modulus
+# e = encryption exponent (assumes gcd(e, phi(n) = 1 and e < n))
+def encrypt(msg, n, e):
+    msg_clean = list(filter(lambda l: l.isalpha(), msg.strip().lower()))
+    # a = 1, b = 2, etc.
+    msg_nums = ''.join(map(lambda l: format(ord(l) - 96, '02'), msg_clean))
+    m = int(msg_nums)
+    return pow(m, e, n)
+
+def num_to_alpha(n):
+    s = str(n)
+    if len(s) % 2 != 0:
+        s = "0" + s
+    digits = []
+    for i in range(0, len(s), 2):
+        digits.append(s[i:i+2])
+    print(digits)
+
+
+def main():
+    if len(sys.argv) != 4:
+        print('bad argument')
+        print('rsa.py plaintext modulus exponent')
+        return
+    print(encrypt(sys.argv[1], int(sys.argv[2]), int(sys.argv[3])))
+
+if __name__ == '__main__':
+    main()
