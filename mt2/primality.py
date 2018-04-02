c4cb53ba4c08ef269f66e7cde9267ac1496e75bd
Bridger Herman <scooteeroo@gmail.com>
2018-04-01 20:55:54 -0500

    Add strong pseudoprimality

--- mt2/primality.py
+++ mt2/primality.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../mt1')
-from fastexp import modexp
+from fastexp import modexp, exp
+from math import log2
 
 def fermat(n):
     for x in range(1, n):
@@ -10,3 +11,17 @@ def fermat(n):
             return False
         else:
             print('Pseudoprime, base', x)
+
+def strong_pseudo(b, n):
+    for m in range(1, n):
+        r = int(log2((n - 1)//m))
+        e = modexp(b, m, n)
+        for s in range(1, r):
+            e2 = modexp(b, exp(2, s)*m, n)
+            if e == 1 or e2 == -1:
+                print('Strong pseudoprime m={}, r={}, s={}'.format(m, r, s),
+                        end=" ")
+                if e == 1:
+                    print('(condition 1)')
+                if e == -1:
+                    print('(condition 2)')
