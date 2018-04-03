3b583473cbb588e52e6ebe242797ff3e0228defd
Bridger Herman <scooteeroo@gmail.com>
2018-04-02 22:19:32 -0500

    Add Miller-Rabin test

---
+++ mt2/miller_rabin.py
@@ -0,0 +1,44 @@
+# Miller-Rabin Test
+# Bridger Herman
+# April 2018
+
+import sys
+sys.path.append('../mt1')
+
+from math import log2, gcd
+from fastexp import exp
+from random import randint
+
+# False = composite
+# True = probably prime
+def miller_rabin(n, x=None):
+    # Get o (odd)
+    j = 1
+    k = 0
+    found = False
+    while j < n - 1 and not found:
+        j <<= 1
+        for o in range(n - 1):
+            if o*j == n - 1 and o % 2 != 0:
+                found = True
+                break
+        k += 1
+    if not found:
+        raise ValueError('Odd number not found')
+
+    # Choose an x
+    if x is None:
+        x = randint(1, n - 1)
+        while gcd(x, n) != 1:
+            x = randint(1, n - 1)
+        print('base', x)
+
+    # Compute M-R sequence (last term first)
+    mr_sequence = (exp(x, (n-1)/(1<<i)) for i in range(k))
+    for term in mr_sequence:
+        m = term % n
+        if m != 1 and m != -1:
+            return False
+        elif m != -1:
+            return True
+    return True
